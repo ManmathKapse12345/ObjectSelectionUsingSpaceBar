009678d347f872a71c56ca751fbe9355190a0f67
ManmathKapse12345 <manmathkapse9@gmail.com>
2026-05-29 13:56:58 +0530

    adding glowness to first object by default !
    i am trying to test whether it works for one object or not

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
+    const int numberOfObjects = 5;
+    public GameObject[] objects = new GameObject[numberOfObjects];
+    int index;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        index = 0;
+        Renderer rend = objects[index].GetComponent<Renderer>();
+        Material mat = rend.material;
+        mat.EnableKeyword("_EMISSION");
+        mat.SetColor("_EmissionColor",Color.green*5.0f);
     }
 
     // Update is called once per frame
