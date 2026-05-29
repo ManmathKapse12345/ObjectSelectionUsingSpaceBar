9b075a504c7f2d315753615dffda0d2f009efc8c
ManmathKapse12345 <manmathkapse9@gmail.com>
2026-05-29 14:53:05 +0530

    Adding same rotation feature to all objects !

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SelectionManager : MonoBehaviour
 {
     const int numberOfObjects = 5;
+    const float speed = 10f;
     public GameObject[] objects = new GameObject[numberOfObjects];
     int index;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,6 +20,10 @@ public class SelectionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for(int i = 0; i < numberOfObjects; i++)
+        {
+            objects[i].transform.Rotate(Vector3.right*Time.deltaTime*speed);
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Renderer rend = objects[index].GetComponent<Renderer>();
