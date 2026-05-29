8306a86e4899089a1c19cf73214e7135d029f679
ManmathKapse12345 <manmathkapse9@gmail.com>
2026-05-29 17:55:15 +0530

    I have added label to each object !
    I have also changed the direction of rotation as well !

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -1,20 +1,34 @@
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class SelectionManager : MonoBehaviour
 {
     private const int numberOfObjects = 5;
+    public Canvas canv;
+    private TMP_Text label;
     private const float speed = 10f;
     public GameObject[] objects = new GameObject[numberOfObjects];
+    private Vector3[] labelPosition = new Vector3[]
+    {
+        new Vector3(-8.64f,-2.42f,-1.57f),
+        new Vector3(-5.48f,-2.42f,-1.57f),
+        new Vector3(-2.3f,-2.42f,-1.57f),
+        new Vector3(1.37f,-2.42f,-1.57f),
+        new Vector3(7f,-2.42f,-1.57f)
+    };
     int index;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        label = canv.GetComponentInChildren<TMP_Text>();
         index = 0;
         Renderer rend = objects[index].GetComponent<Renderer>();
         Material mat = rend.material;
         mat.EnableKeyword("_EMISSION");
         mat.SetColor("_EmissionColor", Color.green * 5.0f);
         Debug.Log(objects[index].name);
+        canv.transform.position = labelPosition[index];
+        label.text = objects[index].name;
     }
 
     // Update is called once per frame
@@ -32,7 +46,7 @@ public class SelectionManager : MonoBehaviour
             }
             else
             {
-                objects[i].transform.Rotate(Vector3.right * Time.deltaTime * speed);
+                objects[i].transform.Rotate(Vector3.left * Time.deltaTime * speed);
             }
         }
         if (Input.GetKeyDown(KeyCode.Space))
@@ -47,6 +61,8 @@ public class SelectionManager : MonoBehaviour
             mat.EnableKeyword("_EMISSION");
             mat.SetColor("_EmissionColor", Color.green * 5.0f);
             Debug.Log(objects[index].name);
+            canv.transform.position = labelPosition[index];
+            label.text = objects[index].name;
         }
 
     }
